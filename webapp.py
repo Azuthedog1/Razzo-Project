aa281bcd4df5c4b114a519490c082f0605b94f22
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-05-10 09:54:43 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,3 +14,7 @@ def render_english_learner_forum():
 @app.route('/specialeducationforum')
 def render_special_education_forum():
     return render_template('specialeducationforum.html')
+
+@app.route('/loginforum')
+def render_login_forum():
+    return render_template('login.html')
