594cda1df0d6eaf70ba0649d8b00ef06c129a5dd
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-10 09:36:21 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,5 +4,13 @@ from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def renderMain():
+def render_home():
     return render_template('home.html')
+
+@app.route('/englishlearnerforum')
+def render_english_learner_forum():
+    return render_template('englishlearnerforum.html')
+
+@app.route('/graph')
+def render_special_education_forum():
+    return render_template('specialeducationforum.html')
