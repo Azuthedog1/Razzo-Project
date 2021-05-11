46d60c75078cd9becce5ed24750306643f839727
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-05-11 09:58:42 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,64 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+from flask import render_template
+
+import pprint
 import os
-from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
+
+app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
+oauth = OAuth(app)
+oauth.init_app(app) #initialize the app to be able to make requests for user information
+
+#Set up GitHub as OAuth provider
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], 
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
+    request_token_params={'scope': 'user:email'}, 
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',  
+    authorize_url='https://github.com/login/oauth/authorize' 
+)
+
+@app.context_processor
+def inject_logged_in():
+    return {"logged_in":('github_token' in session)}
 
 @app.route('/')
 def render_information():
     return render_template('information.html')
+
+@app.route('/login')
+def login():   
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('login.html', message='You were logged out')
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+    if resp is None:
+        session.clear()
+        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+    else:
+        try:
+            session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
+            session['user_data']=github.get('user').data
+            #pprint.pprint(vars(github['/email']))
+            #pprint.pprint(vars(github['api/2/accounts/profile/']))
+            message='You were successfully logged in as ' + session['user_data']['login'] + '.'
+        except Exception as inst:
+            session.clear()
+            print(inst)
+            message='Unable to login, please try again.  '
+    return render_template('login.html', message=message)
 
 @app.route('/englishlearnerforum')
 def render_english_learner_forum():
@@ -15,6 +68,3 @@ def render_english_learner_forum():
 def render_special_education_forum():
     return render_template('specialeducationforum.html')
 
-@app.route('/login')
-def render_login_forum():
-    return render_template('login.html')
