fbc88c5227853d27b697e173ce9fb8f688fe1d6a
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-05-14 09:12:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -55,7 +55,7 @@ def authorized():
             session['user_data']=github.get('user').data
             #pprint.pprint(vars(github['/email']))
             #pprint.pprint(vars(github['api/2/accounts/profile/']))
-            if session['user_data']['login'] == "DanaLearnsToCode" or session['user_data']['login'] == "MyDSWAccount":
+            if session['user_data']['login'] == "DanaLearnsToCode" or session['user_data']['login'] == "MyDSWAccount" or session['user_data']['login'] == "Azuthedog1":
                 message='You were successfully logged in as ' + session['user_data']['login'] + '.'
             else:
                 session.clear()
