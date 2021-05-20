123ec8b7954e46e5dcc5556dcf0df8b03f94a696
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-19 21:22:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -84,21 +84,21 @@ def render_english_learner_forum():
     bigString1 = ""
     for post in collection.find():
         if(post.get('approved') == "false"):
-            bigString1 = bigString1 + Markup({% if logged_in %})
+            bigString1 = bigString1 + Markup('{% if logged_in %}')
         bigString1 = bigString1 + Markup ('<tr><td class="col1">NoIcons</td>')  
         bigString1 = bigString1 + Markup('<td class="col2"><form action="/comments"><select class="selection" name="thread"><option value="' + str(post.get('_id')) + '"></option></select><button type="submit" class="customButton commentButton">' + post.get('postTitle') + '</button></form></td>')
         if(post.get('anonymous') == "true"):
-            bigString1 = bigString1 + Markup('<td class="col3">' + {% if logged_in %} + post.get('parentName') + ', ' + post.get('studentName+grade') + {% endif %} + '(Anonymous)</td>')
+            bigString1 = bigString1 + Markup('<td class="col3"> {% if logged_in %}' + post.get('parentName') + ', ' + post.get('studentName+grade') + '{% endif %} (Anonymous)</td>')
         else:
-            bigString1 = bigString1 + Markup('<td class="col3">' + post.get('parentName') + {% if logged_in %} + ', ' + post.get('studentName+grade') + {% endif %} + '</td>')
-        bigString1 = bigString1 + Markup('<td class="col4"><span>' + {% if logged_in %} + '<form action="/delete" method="post"><button type="submit" class="btn btn-danger btn-sm" name="delete" value="' + str(post.get('_id')) + '">Confirm Delete</button></form><form action="/vet" method="post"><button type="submit" class="btn btn-warning btn-sm" name="vet" value="' + str(post.get('_id')) + '">')
+            bigString1 = bigString1 + Markup('<td class="col3">' + post.get('parentName') + '{% if logged_in %}, ' + post.get('studentName+grade') + '{% endif %}</td>')
+        bigString1 = bigString1 + Markup('<td class="col4"><span>{% if logged_in %}<form action="/delete" method="post"><button type="submit" class="btn btn-danger btn-sm" name="delete" value="' + str(post.get('_id')) + '">Confirm Delete</button></form><form action="/vet" method="post"><button type="submit" class="btn btn-warning btn-sm" name="vet" value="' + str(post.get('_id')) + '">')
         if(post.get('approved') == "false"):
             bigString1 = bigString1 + Markup('Vet')
         else:
             bigString1 = bigString1 + Markup('Unvet')
-        bigString1 = bigString1 + Markup('</button></form>' + {% endif %} + str(post.get('date+time')) + '</span></td></tr>')
+        bigString1 = bigString1 + Markup('</button></form>' + '{% endif %}' + str(post.get('date+time')) + '</span></td></tr>')
         if(post.get('approved') == "false"):
-            bigString1 = bigString1 + Markup({% endif %})
+            bigString1 = bigString1 + Markup('{% endif %}') 
         postList1.insert(0, bigString1)
         bigString1 = ""
     for item in postList1:
