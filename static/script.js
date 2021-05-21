95d8135a6f400526e713135b3a6820bc529aa6ca
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-20 20:43:20 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,14 +1,1 @@
-$(document).ready(function() {
-var form = document.querySelector('form');
-form.onsubmit = function() {
-  // Populate hidden form on submit
-  var about = document.querySelector('input[name=about]');
-  about.value = JSON.stringify(quill.getContents());
-  
-  console.log("Submitted", $(form).serialize(), $(form).serializeArray());
-  
-  // No back end to actually submit to!
-  alert('Open the console to see the submit data!')
-  return false;
-};
-});
+
