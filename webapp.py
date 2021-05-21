5c849f9449fef9c18534caba4cf1488e3167565f
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-21 10:31:00 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -220,7 +220,7 @@ def renderUserPostSubmissionELL():
     #session['userStudent']=request.form['userStudent']
     #session['userEmail']=request.form['userEmail']
     anonymous = "true"
-    if request.form.getlist('anonymous'):
+    if request.form['anonymous']:
         anonymous = "true"
     else:
         anonymous = "false"
@@ -277,7 +277,7 @@ def renderUserPostSubmissionSE():
     #session['userStudent']=request.form['userStudent']
     #session['userEmail']=request.form['userEmail']
     anonymous = "true"
-    if request.form.getlist('anonymous'):
+    if request.form['userAnon']: #getList
         anonymous = "true"
     else:
         anonymous = "false"
