cd3fb079d3d907a6bfcc8777263bd9be015da17e
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-20 19:38:11 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,24 @@
+var quill = new Quill('#editor-container', {
+  modules: {
+    toolbar: [
+      ['bold', 'italic'],
+      ['link', 'blockquote', 'code-block', 'image'],
+      [{ list: 'ordered' }, { list: 'bullet' }]
+    ]
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow'
+});
 
+var form = document.querySelector('form');
+form.onsubmit = function() {
+  // Populate hidden form on submit
+  var about = document.querySelector('input[name=about]');
+  about.value = JSON.stringify(quill.getContents());
+  
+  console.log("Submitted", $(form).serialize(), $(form).serializeArray());
+  
+  // No back end to actually submit to!
+  alert('Open the console to see the submit data!')
+  return false;
+};
