f9f4640d4ed1e13f3d42a5f66a67037b73610d0e
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-20 20:12:04 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,26 +1,3 @@
-var toolbarOptions = [
-  ['bold', 'italic', 'underline', 'strike'],
-  ['link'],
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
-
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ 'align': [] }],
-
-  ['clean']                                         // remove formatting button
-];
-
-var quill = new Quill('#editor-container', {
-  modules: {
-    toolbar: toolbarOptions
-  },
-  theme: 'snow'
-});
-
 var form = document.querySelector('form');
 form.onsubmit = function() {
   // Populate hidden form on submit
