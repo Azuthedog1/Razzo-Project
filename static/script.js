531c5b070f6daf1aac250743881329264ba3faf0
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-20 18:58:00 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,6 @@
+var justHtmlContent = document.getElementById('justHtml');
 
+editor.on('text-change', function() {
+  var justHtml = editor.root.innerHTML;
+  justHtmlContent.innerHTML = justHtml;
+});
