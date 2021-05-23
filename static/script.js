918366655f38c19c864a15ec0a4908a946a056bb
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-22 18:52:42 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,2 +1,12 @@
 $(document).ready(function() {
+  var form = document.querySelector('form');
+  form.onsubmit = function() {
+    var about = document.querySelector('input[name=about]');
+    about.value = JSON.stringify(quill.getContents());
+  
+    console.log("Submitted", $(form).serialize(), $(form).serializeArray());
+  
+    alert('Open the console to see the submit data!')
+    return false;
+  };
 });
