fdef5f9b5b07543f50c2f9289b7ede587f51730d
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-22 19:36:39 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
   var form = document.querySelector('#adminform'); 
   form.onsubmit = function() { 
     var about = document.querySelector('input[name=adminComment]'); 
-    about.value = JSON.stringify(quill.getContents()); 
+    about.value = editor.root.innerHTML;
   }; 
 });
