8605530096900de148893cb15157248b3d61d025
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-22 19:29:25 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,7 +12,7 @@ from pytz import timezone
 import pytz
 
 utc = pytz.utc
-eastern = timezone('US/Eastern')
+eastern = timezone('America/Los_Angeles')
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
 app = Flask(__name__)
