75a6dd43114970f43288a19346c83f713d6cfe50
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-22 19:02:12 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var form = document.querySelector('form');
   form.onsubmit = function() {
-    var about = document.querySelector('input[name=about]');
+    var about = document.querySelector('input[name=adminComment]');
     about.value = JSON.stringify(quill.getContents());
   
     console.log("Submitted", $(form).serialize(), $(form).serializeArray());
