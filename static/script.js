8c72fe1108ae0c3e707a1f32f168566b86bf0d2c
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-05-22 19:14:15 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var form = document.querySelector('form');
+  var form = document.querySelector('#adminform');
   form.onsubmit = function() {
     var about = document.querySelector('input[name=adminComment]');
     about.value = JSON.stringify(quill.getContents());
