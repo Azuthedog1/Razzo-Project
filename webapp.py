b0b3b0b6b9f708cd722dbb0a805154e53982e7c8
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-23 22:06:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -452,6 +452,7 @@ def viewSEU(objectID):
     except: 
         pass
     if(objectIDPost == ""):
+        objectIDPost = request.args['thread']
     connection_string = os.environ["MONGO_CONNECTION_STRING"]
     db_name = os.environ["MONGO_DBNAME"]
     client = pymongo.MongoClient(connection_string)
