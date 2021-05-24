7a193452a468df7ca75530da3c69a18140cf5a5a
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-23 21:40:33 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -427,7 +427,7 @@ def viewSEA():
                             bigString += '<tr><td class="comments"><b>' + post.get("comment" + str(i), {}).get("parentName") + '</b><br><i>' + loc_dt + '</i><br><br>' + post.get("comment" + str(i), {}).get("postContent") + '</td></tr>'
                 counter += 1
             i += 1
-    return render_template('comments.html', title = postTitle, name = parentName, information = info, time = loc_dt, content = Markup(postContent), ID = objectIDPost, comments = Markup(bigString))
+    return render_template('comments.html', title = postTitle, name = displayName, information = info, time = loc_dt, content = Markup(postContent), ID = objectIDPost, comments = Markup(bigString))
 
 @app.route('/viewSEU')
 def viewSEU():
@@ -578,7 +578,7 @@ def viewELLA():
                             bigString += '<tr><td class="comments"><b>' + post.get("comment" + str(i), {}).get("parentName") + '</b><br><i>' + loc_dt + '</i><br><br>' + post.get("comment" + str(i), {}).get("postContent") + '</td></tr>'
                 counter += 1
             i += 1
-    return render_template('comments.html', title = postTitle, name = parentName, information = info, time = loc_dt, content = Markup(postContent), ID = objectIDPost, comments = Markup(bigString))
+    return render_template('comments.html', title = postTitle, name = displayName, information = info, time = loc_dt, content = Markup(postContent), ID = objectIDPost, comments = Markup(bigString))
 
 @app.route('/viewELLU')
 def viewELLU():
