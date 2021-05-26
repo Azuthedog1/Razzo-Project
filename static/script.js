d015d66218b0f7c636c79f3f72b3d683ac1c30f4
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-26 14:20:39 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() { 
-  $('.delete').siblings('.confirm', '.delete').css('diplay", "inline-block');
+  $('.delete').siblings('.confirm', '.delete').css('display", "inline-block');
   var form = document.querySelector('#adminform'); 
   form.onsubmit = function() { 
     var about = document.querySelector('input[name=adminMessage]'); 
