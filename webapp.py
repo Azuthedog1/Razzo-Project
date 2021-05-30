2bd2f4b9c8f047353c3e15eea2873c4ae3dde013
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-05-29 17:10:25 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -82,43 +82,50 @@ def authorized():
     return render_template('login.html', message = message)
 
 def send_email(receiver_email, title, name, link):
-    smtp_server = 'smtp.gmail.com'
-    sender_email = 'sbhsparentboard@gmail.com'
-    password = 'PBh5inLgFKvD'
-    message = MIMEMultipart('alternative')
-    message['Subject'] = 'SBHS Parent Board Notification'
-    message['From'] = sender_email
-    message['To'] = receiver_email
-    text = """\
-    Hello name,
-    Your post title has recieved a response from a staff member.
-    <link>
-    
-    Hola name,
-    Tu publicación title ha recibido una respuesta de un miembro del personal.
-    <link>"""
-    html = """\
-    <html>
-        <body>
-            <p><b>Hi, """ + name + """.</b><br>
-            Your post <a href='""" + link + """'>""" + title + """</a> has recieved a response from a staff member.<br>
-            --------------------------------------------------------------------------------------------------------<br>
-            <b>Hola, """ + name + """. </b><br>
-            Tu publicación <a href='""" + link + """'>""" + title + """</a> ha recibido una respuesta de un miembro del personal.<br>
-            --------------------------------------------------------------------------------------------------------<br>
-            <small>*Please do not response to this email / Por favor, no responda a este correo electrónico.</small>
-            </p>
-        </body>
-    </html>
-    """
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
-    message.attach(part1)
-    message.attach(part2)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+    connection_string = os.environ['MONGO_CONNECTION_STRING']
+    db_name = os.environ['MONGO_DBNAME']
+    client = pymongo.MongoClient(connection_string)
+    db = client[db_name]
+    collection = db['EMAIL']
+    try:
+        information = collection.find_one({'_id': ObjectId('60b2d66ba55f630f74e0a554')})
+        smtp_server = 'smtp.gmail.com'
+        sender_email = information.get('sender_email')
+        password = information.get('password')
+        message = MIMEMultipart('alternative')
+        message['Subject'] = 'SBHS Parent Board Notification'
+        message['From'] = sender_email
+        message['To'] = receiver_email
+        text = """\
+        Hello name,
+        Your post title has recieved a response from a staff member.
+        <link>
+        
+        Hola name,
+        Tu publicación title ha recibido una respuesta de un miembro del personal.
+        <link>"""
+        html = """\
+        <html>
+            <body>
+                <p><b>Hi, """ + name + """.</b><br>
+                Your post <a href='""" + link + """'>""" + title + """</a> has recieved a response from a staff member.<br>
+                --------------------------------------------------------------------------------------------------------<br>
+                <b>Hola, """ + name + """. </b><br>
+                Tu publicación <a href='""" + link + """'>""" + title + """</a> ha recibido una respuesta de un miembro del personal.<br>
+                --------------------------------------------------------------------------------------------------------<br>
+                <small>*Please do not response to this email / Por favor, no responda a este correo electrónico.</small>
+                </p>
+            </body>
+        </html>
+        """
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
+        message.attach(part1)
+        message.attach(part2)
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
 
 @app.route('/englishlearnerforum')
 def render_english_learner_forum():
